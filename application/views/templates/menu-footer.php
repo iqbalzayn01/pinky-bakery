d46e22960d2cf11ff5729e7f0990c72669f80957
Iqbal Zayyan <iqbalzayyanabiyyu@gmail.com>
2022-10-24 21:56:53 +0700

    15th commit - access management

--- application/views/templates/menu-footer.php
+++ application/views/templates/menu-footer.php
@@ -49,6 +49,35 @@
     <!-- Custom scripts for all pages-->
     <script src="<?= base_url('assets/js/sb-admin-2.min.js') ?>"></script>
 
+
+    <script>
+        $('.custom-file-input').on('change', function() {
+            let fileName = $(this).val().split('\\').pop();
+            $(this).next('.custom-file-label').addClass("selected").html(fileName);
+        });
+
+
+
+        $('.form-check-input').on('click', function() {
+            const menuId = $(this).data('menu');
+            const roleId = $(this).data('role');
+
+            $.ajax({
+                url: "<?= base_url('admin/changeaccess'); ?>",
+                type: 'post',
+                data: {
+                    menuId: menuId,
+                    roleId: roleId
+                },
+                success: function() {
+                    document.location.href = "<?= base_url('admin/roleaccess/'); ?>" + roleId;
+                }
+            });
+
+        });
+    </script>
+
+
 </body>
 
 </html>
